e8d1f26f8422b2f070afd1dad0e6be4c9ae9525b
David Hamilton <nova706@gmail.com>
2023-03-19 17:00:01 -0600

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,9 @@
-export interface Window {
-    pid: number;
-    windowName: string;
-    process: string;
-}
-
-declare module 'processAddon' {
+declare module '@launch-deck/process-addon' {
     export function focusWindow(pid: number): void;
     export function getWindows(): Window[];
+    export interface Window {
+        pid: number;
+        windowName: string;
+        process: string;
+    }
 }
